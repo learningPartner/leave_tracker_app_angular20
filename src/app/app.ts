414d2268394557705c341484078fe76a290e15e0
learningPartner <learningpartnerdigital@gmail.com>
2025-11-12 11:58:18 +0530

    initial Files

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'leave_tracker_app_angular20';
+}
